f0d8444a44818d0a9552a219048f25c592a61e41
Semyon Levin <levin.semen@gmail.com>
2024-11-19 09:29:47 -0800

    [release-later] Remove `PluginCache` dependency from `Log4j2PluginsMerger`

--- src/main/java/name/remal/gradle_plugins/merge_resources/mergers/Log4j2PluginsMerger.java
+++ src/main/java/name/remal/gradle_plugins/merge_resources/mergers/Log4j2PluginsMerger.java
@@ -1,20 +1,27 @@
 package name.remal.gradle_plugins.merge_resources.mergers;
 
-import static java.util.Collections.enumeration;
-import static java.util.stream.Collectors.toList;
+import static java.nio.file.Files.newInputStream;
+import static java.util.Locale.ROOT;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.nio.file.Path;
 import java.util.Collection;
-import lombok.SneakyThrows;
+import java.util.Map;
+import java.util.TreeMap;
+import lombok.Builder;
+import lombok.Value;
 import lombok.val;
 import name.remal.gradle_plugins.merge_resources.ResourceMerger;
-import org.apache.logging.log4j.core.config.plugins.processor.PluginCache;
 import org.gradle.api.file.RelativePath;
 
+/**
+ * See
+ * <a href="https://github.com/apache/logging-log4j2/blob/caffa21f6409c7e9402d52c98072cf0b68198401/log4j-core/src/main/java/org/apache/logging/log4j/core/config/plugins/processor/PluginCache.java">org.apache.logging.log4j.core.config.plugins.processor.PluginCache</a>.
+ */
 public abstract class Log4j2PluginsMerger extends ResourceMerger {
 
     @Override
@@ -39,17 +46,66 @@ public abstract class Log4j2PluginsMerger extends ResourceMerger {
         Collection<Path> paths,
         OutputStream outputStream
     ) throws Throwable {
-        val pluginCache = new PluginCache();
+        val categories = new TreeMap<String, Map<String, Log4jPlugin>>();
 
-        val urls = paths.stream().map(Log4j2PluginsMerger::pathToUrl).collect(toList());
-        pluginCache.loadCacheFiles(enumeration(urls));
+        for (val path : paths) {
+            try (val in = new DataInputStream(newInputStream(path))) {
+                val categoriesCount = in.readInt();
+                for (int categoryNumber = 1; categoryNumber <= categoriesCount; categoryNumber++) {
+                    val category = in.readUTF();
+                    val plugins = categories.computeIfAbsent(
+                        category.toLowerCase(ROOT),
+                        __ -> new TreeMap<>()
+                    );
 
-        pluginCache.writeCache(outputStream);
+                    val pluginsCount = in.readInt();
+                    for (int pluginNumber = 1; pluginNumber <= pluginsCount; pluginNumber++) {
+                        val key = in.readUTF();
+                        val className = in.readUTF();
+                        val name = in.readUTF();
+                        val printable = in.readBoolean();
+                        val defer = in.readBoolean();
+                        plugins.computeIfAbsent(key, currentKey ->
+                            Log4jPlugin.builder()
+                                .key(key)
+                                .className(className)
+                                .name(name)
+                                .printable(printable)
+                                .defer(defer)
+                                .build()
+                        );
+                    }
+                }
+            }
+        }
+
+        try (val out = new DataOutputStream(outputStream)) {
+            out.writeInt(categories.size());
+            for (val categoryEntry : categories.entrySet()) {
+                out.writeUTF(categoryEntry.getKey());
+
+                val plugins = categoryEntry.getValue();
+                out.writeInt(plugins.size());
+                for (val pluginEntry : plugins.entrySet()) {
+                    val plugin = pluginEntry.getValue();
+                    out.writeUTF(plugin.getKey());
+                    out.writeUTF(plugin.getClassName());
+                    out.writeUTF(plugin.getName());
+                    out.writeBoolean(plugin.isPrintable());
+                    out.writeBoolean(plugin.isDefer());
+                }
+            }
+        }
     }
 
-    @SneakyThrows
-    private static URL pathToUrl(Path path) {
-        return path.toUri().toURL();
+    @Value
+    @Builder
+    private static class Log4jPlugin {
+        String key;
+        String className;
+        String name;
+        boolean printable;
+        boolean defer;
     }
 
 }
--- src/test/java/name/remal/gradle_plugins/merge_resources/mergers/Log4j2PluginsMergerTest.java
+++ src/test/java/name/remal/gradle_plugins/merge_resources/mergers/Log4j2PluginsMergerTest.java
@@ -3,20 +3,21 @@ package name.remal.gradle_plugins.merge_resources.mergers;
 import static java.lang.ClassLoader.getSystemClassLoader;
 import static java.nio.file.FileSystems.newFileSystem;
 import static java.util.Collections.emptyMap;
+import static java.util.Collections.enumeration;
 import static java.util.Collections.list;
 import static java.util.stream.Collectors.toList;
 import static name.remal.gradle_plugins.toolkit.SneakyThrowUtils.sneakyThrows;
 import static name.remal.gradle_plugins.toolkit.StringUtils.substringAfter;
 import static name.remal.gradle_plugins.toolkit.StringUtils.substringBefore;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.io.ByteArrayOutputStream;
 import java.net.URI;
-import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Optional;
 import lombok.val;
+import name.remal.gradle_plugins.toolkit.UriUtils;
+import org.apache.logging.log4j.core.config.plugins.processor.PluginCache;
 import org.junit.jupiter.api.Test;
 
 class Log4j2PluginsMergerTest {
@@ -27,12 +28,12 @@ class Log4j2PluginsMergerTest {
 
     @Test
     @SuppressWarnings("resource")
-    void autoScenario() throws Throwable {
+    void equalToPluginCache() throws Throwable {
         val urls = list(RESOURCE_LOADER
             .getResources("META-INF/org/apache/logging/log4j/core/config/plugins/Log4j2Plugins.dat")
         );
         val paths = urls.stream()
-            .map(sneakyThrows(URL::toURI))
+            .map(UriUtils::toUri)
             .map(sneakyThrows((URI uri) -> {
                 val file = substringBefore(uri.toString(), "!", "");
                 if (file.isEmpty()) {
@@ -48,11 +49,23 @@ class Log4j2PluginsMergerTest {
         assertThat(paths).as("paths")
             .hasSizeGreaterThanOrEqualTo(3);
 
+        final byte[] expectedBytes;
         try (val outputStream = new ByteArrayOutputStream()) {
-            assertDoesNotThrow(() ->
-                Log4j2PluginsMerger.mergeLog4j2PluginsTo(paths, outputStream)
-            );
+            val pluginCache = new PluginCache();
+            pluginCache.loadCacheFiles(enumeration(urls));
+            pluginCache.writeCache(outputStream);
+            expectedBytes = outputStream.toByteArray();
         }
+
+        final byte[] actualBytes;
+        try (val outputStream = new ByteArrayOutputStream()) {
+            Log4j2PluginsMerger.mergeLog4j2PluginsTo(paths, outputStream);
+            actualBytes = outputStream.toByteArray();
+        }
+
+        assertThat(actualBytes)
+            .isNotEmpty()
+            .isEqualTo(expectedBytes);
     }
 
 }
